494f7f9b235639b6b3e6cea4779abcb4f85bc20f
Jyothi-Tirumani <jothi.tirumani@outlook.com>
2023-07-22 11:38:41 +0530

    version1

---
+++ Laptop.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  document.getElementById("panel").style.display = "block";
+}
+
+function myFunction1() {
+  document.getElementById("panel1").style.display = "block";
+}
+
+function myFunction2() {
+  document.getElementById("panel2").style.display = "block";
+}
+
+function myFunction3() {
+  document.getElementById("panel3").style.display = "block";
+}
